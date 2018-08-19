43ff6aa4652f8361e4b470ce017c7b6344b82da5
android <android.dev.en@yandex.ru>
2018-08-19 19:33:32 +0300

    add main listview click listner

--- app/src/main/java/inc/aminkinen/foreigncards/MainActivity.kt
+++ app/src/main/java/inc/aminkinen/foreigncards/MainActivity.kt
@@ -2,10 +2,12 @@ package inc.aminkinen.foreigncards
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
+import android.view.View
+import android.widget.*
+import android.content.Intent
+import android.widget.TextView
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         list.adapter = ArrayAdapter(this,
                 android.R.layout.simple_list_item_1, arrayOf(TrainMenuText, AddingMenuText, GroupsMenuText, SettingsMenuText))
+
+        list.setOnItemClickListener { _: AdapterView<*>, view: View, _: Int, _: Long ->
+            val textView = view as? TextView ?: return@setOnItemClickListener
+            when (textView.text.toString()) {
+                TrainMenuText -> return@setOnItemClickListener // startActivity(Intent(this, BarsikActivity::class.java))
+                AddingMenuText -> return@setOnItemClickListener
+                GroupsMenuText -> return@setOnItemClickListener
+                SettingsMenuText -> return@setOnItemClickListener
+            }
+        }
     }
 
     companion object {
