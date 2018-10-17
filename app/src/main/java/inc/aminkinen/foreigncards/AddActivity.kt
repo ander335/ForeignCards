6a4b39aa38125a94570f41b6a664ad9ef4b0b54f
android <android.dev.en@yandex.ru>
2018-10-17 21:00:24 +0300

    AddActivity: add code

--- app/src/main/java/inc/aminkinen/foreigncards/AddActivity.kt
+++ app/src/main/java/inc/aminkinen/foreigncards/AddActivity.kt
@@ -3,12 +3,50 @@ package inc.aminkinen.foreigncards
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.*
-import android.widget.TextView
+import android.content.Intent
+import android.util.Log
+import android.widget.EditText
+import inc.aminkinen.foreigncards.database.DbProvider
+import inc.aminkinen.foreigncards.entities.CardData
+
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val groupIdForAdding = intent.getIntExtra("group_id_for_adding", 0)
+        Log.i("AddActivity", "Group id for adding: $groupIdForAdding")
+
+        val word = findViewById<EditText>(R.id.text_word)
+        val transl = findViewById<EditText>(R.id.text_transl)
+        val transc = findViewById<EditText>(R.id.text_transc)
+
+        val close = findViewById<View>(R.id.button_close)
+        close.setOnClickListener {
+            Log.i("AddActivity", "Close button was clicked")
+            goToMain()
+        }
+
+        val add = findViewById<View>(R.id.button_add)
+        add.setOnClickListener {
+            Log.i("AddActivity", "Add button was clicked")
+            val c = CardData(
+                    word.text.toString(),
+                    transl.text.toString(),
+                    transc.text.toString(),
+                    "", // TODO: example
+                    groupIdForAdding)
+
+            Log.i("AddActivity", "Add card: $c")
+            DbProvider.Instance.addCard(c)
+            goToMain()
+        }
+    }
+
+    private fun goToMain() {
+        Log.i("AddActivity", "Switch to main activity")
+        val i = Intent(this@AddActivity, MainActivity::class.java)
+        startActivity(i)
     }
 }
--- app/src/main/java/inc/aminkinen/foreigncards/entities/CardData.kt
+++ app/src/main/java/inc/aminkinen/foreigncards/entities/CardData.kt
@@ -4,4 +4,8 @@ open class CardData(val Word: String = "",
                     val Transl: String = "",
                     val Transc: String = "",
                     val Example: String = "",
-                    val GroupId: Int = -1)
+                    val GroupId: Int = -1) {
+    override fun toString() : String {
+        return "$GroupId. $Word ($Transc) - $Transl ($Example)"
+    }
+}
