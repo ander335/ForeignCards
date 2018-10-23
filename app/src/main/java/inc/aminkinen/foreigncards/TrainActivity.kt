ecfaa7a93b2da242e0959b09e8a208af1ca6f238
android <android.dev.en@yandex.ru>
2018-10-23 21:08:28 +0300

    ActivityTrain: add Navigator prototype

--- app/src/main/java/inc/aminkinen/foreigncards/TrainActivity.kt
+++ app/src/main/java/inc/aminkinen/foreigncards/TrainActivity.kt
@@ -11,26 +11,19 @@ import inc.aminkinen.foreigncards.database.DbProvider
 import inc.aminkinen.foreigncards.entities.Card
 import inc.aminkinen.foreigncards.entities.Settings
 
-
 class TrainActivity : AppCompatActivity() {
     private val _db : DbProvider = DbProvider.Instance
     private val _settings : Settings = _db.getSettings()
-    private val _cards : ArrayList<Card> = _db.getCards(_settings.GroupIdForTraining)
 
-    private var _currIdx : Int = -1
-    private val _currCard : Card
-        get() = if (_currIdx >= 0 && _currIdx < _cards.size) _cards[_currIdx]
-                else throw Exception("Incorrect index")
+    private val _cards : Navigator = Navigator(this, _db.getCards(_settings.GroupIdForTraining))
+    private val _currCard : Card get() = _cards.currCard
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_train)
 
+        _cards.setup()
         setupMoving()
-        setupNavigation()
-
-
-        //val adding = findViewById<TextView>(R.id.text_group_for_adding)
 
         val close = findViewById<Button>(R.id.at_button_close)
         close.setOnClickListener {
@@ -50,39 +43,55 @@ class TrainActivity : AppCompatActivity() {
         move2.text = String.format(resources.getString(R.string.at_button_move_to_template), group2)
 
         val moveFunc = { group : Int ->
-            if (_cards.size == 0) {
-                Log.i("TrainActivity", "Move button was clicked, nothing to do")
-            } else {
-                Log.i("TrainActivity", "Move button was clicked, move to $group")
-                _currCard.GroupId = group
-                _db.updateCard(_currCard)
-            }
+            Log.i("TrainActivity", "Move button was clicked, move to $group")
+            _currCard.GroupId = group
+            _db.updateCard(_currCard)
         }
 
         move1.setOnClickListener { moveFunc(group1) }
         move2.setOnClickListener { moveFunc(group2) }
+
+        // TODO: go on
     }
 
-    private fun setupNavigation() {
-        val textMin = findViewById<TextView>(R.id.text_num_min)
-        val textMax = findViewById<TextView>(R.id.text_num_max)
-        val textCurr = findViewById<TextView>(R.id.text_num_curr)
-
-        textMin.text = "1"
-        textCurr.text = "1"
-        textMax.text = _cards.size.toString()
-
-        _currIdx = 0;
-
-        val seekBar = findViewById<SeekBar>(R.id.seek_bar_position)
-        seekBar.max = _cards.size - 1
-        seekBar.progress = _currIdx
+    private fun onCurrCardChanged() {
+        // TODO: setup curr cards
     }
-
 
     private fun goToMain() {
         Log.i("TrainActivity", "Switch to main activity")
         val i = Intent(this@TrainActivity, MainActivity::class.java)
         startActivity(i)
+    }
+
+    class Navigator(private val _ctx : TrainActivity, private val _cards : ArrayList<Card>) {
+        private var _currIdx : Int = -1
+        private val _cardsCount : Int = _cards.size
+
+        val currCard : Card
+            get() = if (_currIdx >= 0 && _currIdx < _cards.size) _cards[_currIdx]
+                    else throw Exception("Incorrect index")
+
+        private val _textMin = _ctx.findViewById<TextView>(R.id.text_num_min)
+        private val _textMax = _ctx.findViewById<TextView>(R.id.text_num_max)
+        private val _textCurr = _ctx.findViewById<TextView>(R.id.text_num_curr)
+
+        private val _seekBar = _ctx.findViewById<SeekBar>(R.id.seek_bar_position)
+        private val _buttonPrev = _ctx.findViewById<Button>(R.id.at_button_prev)
+        private val _buttonNext = _ctx.findViewById<Button>(R.id.at_button_next)
+
+        fun setup() {
+            _currIdx = 0
+            _seekBar.max = _cardsCount - 1
+            _seekBar.progress = 0
+
+            _textMin.text = "1"
+            _textCurr.text = "1"
+            _textMax.text = _cards.size.toString()
+
+            // TODO: setup events here
+
+            _ctx.onCurrCardChanged()
+        }
     }
 }
